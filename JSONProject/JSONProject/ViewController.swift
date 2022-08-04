5b1ae3a1aae558e1ef850847c81fa7d9d80c7bd1
nithisha <nithisre@amazon.com>
2022-08-04 09:25:20 -0500

    json

--- JSONProject/JSONProject/ViewController.swift
+++ JSONProject/JSONProject/ViewController.swift
@@ -9,75 +9,45 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource
 {
-    struct jsondata: Codable{
-        var userId: Int?
-        var id: Int?
-        var title : String?
-        var body : String?
+    struct jsondata: Any{
+        var userId: Any?
+        var id: Any?
+        var title : Any?
+        var body : Any?
         }
+    var z = [Any]()
+    var tableView = UITableView(frame: UIScreen.main.bounds, style: .grouped)
 
-   private var tableview: UITableView!
-    var z = [jsondata]()//= [jsondata]()
-    
-    override func viewDidLoad() {
+     override func viewDidLoad() {
            super.viewDidLoad()
-
-        tableview = UITableView.init(frame: UIScreen.main.bounds, style: .grouped)
-        tableview.delegate = self
-        tableview.dataSource = self
-        readLocalFile(forName: "datafile")
-      
-        
-        
-        self.view.addSubview(tableview)
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+         z = self.readLocalFile(forName: "datafile")
+        self.view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "jsonTVCell")
+        //print(p[0])
        }
-       
-       private func readLocalFile(forName name: String) {
-       
-           let bundlePath = Bundle.main.path(forResource: name, ofType: "json")
-           
-           
-           let filedataString = try! String.init(contentsOfFile: bundlePath!)
-           
-           let data = filedataString.data(using: .utf8)
-           let jsonArray = try! JSONSerialization.jsonObject(with: data!, options: []) as! [jsondata]
-           do {
-               z = try JSONDecoder().decode(jsonArray, from: data!)
-           }
-           catch{
-               print(error)
-           }
-           print(jsonArray)
-           return
-       }
+    private func readLocalFile(forName name: String) ->[Any] {
+    
+        let bundlePath = Bundle.main.path(forResource: name, ofType: "json")
+        let filedataString = try! String.init(contentsOfFile: bundlePath!)
+        let data = filedataString.data(using: .utf8)
+        let jsonArray = try! JSONSerialization.jsonObject(with: data!, options: []) as! [Any]
+        return jsonArray
 
+    }
+     func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
-       // MARK: - Table view data source
-        
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           // #warning Incomplete implementation, return the number of rows
-            return z.count
-       }
-
-       
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "Cell1")
-            cell.textLabel?.font = UIFont(name: "System-Bold", size: 22)
-          //cell.textLabel?.text = options[indexPath.section][indexPath.row]
-            //cell.textLabel?.text = z[indexPath.row].title
-            cell.textLabel?.text = z[indexPath.row].id
-            cell.description = (z[indexPath.row].title + " ," + z[indexPath.row].userId + "," + z[indexPath.row].body)
-                  //  cell.detailTextLabel?.text = z[indexPath.row].id! + " " + z[indexPath.row].body
-            //cell.detailTextLabel?.text = ta
-          //cell.su
-           // cell.description = tableViewModel[indexPath.row].body
-           // cell.contentView = ta
-           return cell
-       }
-
-
-
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "jsonTVCell", for: indexPath)
+        cell.textLabel?.text = z[indexPath.row] as? String
+        return cell
+    }
+     
 }
 
---
+++ customtableviewdemoTVcell/customtableviewdemoTVcell/TableViewdisplay.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customtableviewdemoTVcell
+//
+//  Created by Aennam, Nithisha Reddy on 7/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
